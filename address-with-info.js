d6f7969a6cb262d583eebd94dac2db1ef52350e7
Michael Vellequette <michaelv245@yahoo.com>
2021-02-17 21:03:53 -0700

    Initial commit

---
+++ address-with-info.js
@@ -0,0 +1,13 @@
+export const addressInfo = [
+  {
+    address: '2817 W 33rd Ave',
+    lat: 39.76335056,
+    long: -105.02270926,
+    ownerName: 'UNIQUE REAL ESTATE INVESTMENTS,LLC',
+    allOwnedProperties: [
+      '2817 W 33rd Ave',
+      '2817 W 33rd Ave',
+      '2817 W 33rd Ave',
+    ],
+  },
+];
---
+++ map-manager.js
@@ -0,0 +1,17 @@
+import { addressInfo } from './addresses-with-info.js';
+
+var mymap = L.map('mapid').setView([39.7348, -104.9653], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(mymap);
+
+console.log(addressInfo);
+addressInfo.forEach(({ address, lat, long, ownerName, allOwnedProperties }) => {
+  const marker = L.marker([lat, long]).addTo(mymap);
+  marker.bindPopup(
+    `${address} <br> Owned By: ${ownerName} <br> All Properties Owned: ${allOwnedProperties.join(
+      '<br>'
+    )}`
+  );
+});
