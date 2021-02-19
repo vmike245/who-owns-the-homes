91de801984fe06cb9bdc45c1f943f90594586af1
Michael Vellequette <michaelv245@yahoo.com>
2021-02-18 19:25:45 -0700

    Change all colors to get color to work

--- map-manager.js
+++ map-manager.js
@@ -2,7 +2,7 @@
 
 var mymap = L.map('mapid', { preferCanvas: true }).setView(
   [39.73865208, -104.98546635],
-  17
+  16
 );
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
@@ -16,9 +16,7 @@ addressInfo.forEach((addressWithInfo, index) => {
   try {
     const marker = L.circleMarker([parseFloat(lat), parseFloat(long)], {
       radius: 5,
-      ...(governmentEntities.includes(ownerName) && {
-        color: '#ff000',
-      }),
+      color: '#ff000',
     }).addTo(mymap);
     marker.bindPopup(
       `${address} <br> <strong>Owned By</strong>: ${ownerName} <br> <strong>All Properties Owned</strong>: <br>${
