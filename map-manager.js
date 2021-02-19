68084ff42023606cc735b40da2d70bff946d2b16
Michael Vellequette <michaelv245@yahoo.com>
2021-02-18 19:13:27 -0700

    Color government owned buildings differently

--- map-manager.js
+++ map-manager.js
@@ -2,18 +2,24 @@
 
 var mymap = L.map('mapid', { preferCanvas: true }).setView(
   [39.7348, -104.9653],
-  13
+  20
 );
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(mymap);
 
+var governmentEntities = ['CITY & COUNTY OF DENVER'];
+
 addressInfo.forEach((addressWithInfo, index) => {
   const { address, lat, long, ownerName, allOwnedProperties } = addressWithInfo;
   try {
     const marker = L.circleMarker([parseFloat(lat), parseFloat(long)], {
       radius: 5,
+      ...(governmentEntities.includes(ownerName) && {
+        color: red,
+        opacity: 0.5,
+      }),
     }).addTo(mymap);
     marker.bindPopup(
       `${address} <br> <strong>Owned By</strong>: ${ownerName} <br> <strong>All Properties Owned</strong>: <br>${
