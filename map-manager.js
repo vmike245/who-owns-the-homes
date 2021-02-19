5cdfae9f9b41622cf5000aca5c3fed9bee56e78e
Michael Vellequette <michaelv245@yahoo.com>
2021-02-18 20:48:23 -0700

    Add USA to list of government owners and fix email link

--- map-manager.js
+++ map-manager.js
@@ -1,15 +1,17 @@
-// import { addressInfo } from './addresses-with-info.js';
-
 var mymap = L.map('mapid', { preferCanvas: true }).setView(
   [39.73865208, -104.98546635],
   16
 );
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(mymap);
 
-var governmentEntities = ['CITY & COUNTY OF DENVER'];
+var governmentEntities = [
+  'CITY & COUNTY OF DENVER',
+  'UNITED STATES OF AMERICA',
+];
 
 addressInfo.forEach((addressWithInfo, index) => {
   const { address, lat, long, ownerName, allOwnedProperties } = addressWithInfo;
