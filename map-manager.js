1fcfc8a8265509ac01bf8b83a445abc353bd001a
Michael Vellequette <michaelv245@yahoo.com>
2021-02-18 18:11:10 -0700

    Actually use radius correctly

--- map-manager.js
+++ map-manager.js
@@ -9,15 +9,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(mymap);
 
-console.log(addressInfo);
-addressInfo.forEach((addressWithInfo) => {
+addressInfo.forEach((addressWithInfo, index) => {
   const { address, lat, long, ownerName, allOwnedProperties } = addressWithInfo;
   try {
-    const marker = L.circleMarker([
-      parseFloat(lat),
-      parseFloat(long),
-      { radius: 5 },
-    ]).addTo(mymap);
+    const marker = L.circleMarker([parseFloat(lat), parseFloat(long)], {
+      radius: 5,
+    }).addTo(mymap);
     marker.bindPopup(
       `${address} <br> <strong>Owned By</strong>: ${ownerName} <br> <strong>All Properties Owned</strong>: <br>${
         allOwnedProperties.length > 10
@@ -26,6 +23,6 @@ addressInfo.forEach((addressWithInfo) => {
       }`
     );
   } catch (err) {
-    console.log(addressWithInfo);
+    console.log(index);
   }
 });
