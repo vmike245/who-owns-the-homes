005c577bff7b5906d98c785409ba45e681d3babe
Michael Vellequette <michaelv245@yahoo.com>
2021-02-18 17:58:57 -0700

    Use canvas and markers to help performance

--- map-manager.js
+++ map-manager.js
@@ -1,19 +1,25 @@
 // import { addressInfo } from './addresses-with-info.js';
 
-var mymap = L.map('mapid').setView([39.7348, -104.9653], 13);
+var mymap = L.map('mapid', { preferCanvas: true }).setView(
+  [39.7348, -104.9653],
+  13
+);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(mymap);
 
 console.log(addressInfo);
-addressInfo.forEach(({ address, lat, long, ownerName, allOwnedProperties }) => {
+addressInfo.forEach((addressWithInfo) => {
+  const { address, lat, long, ownerName, allOwnedProperties } = addressWithInfo;
   try {
-    const marker = L.marker([parseFloat(lat), parseFloat(long)]).addTo(mymap);
+    const marker = L.circleMarker([parseFloat(lat), parseFloat(long)]).addTo(
+      mymap
+    );
     marker.bindPopup(
       `${address} <br> <strong>Owned By</strong>: ${ownerName} <br> <strong>All Properties Owned</strong>: <br>${allOwnedProperties.length}`
     );
   } catch (err) {
-    console.log(address);
+    console.log(addressWithInfo);
   }
 });
