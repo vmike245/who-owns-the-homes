270590117c1509427ac86e7d8c5f2f1c2f3466ae
Michael Vellequette <michaelv245@yahoo.com>
2021-02-22 19:36:37 -0700

    Add ability to filter by number of properties

--- map-manager.js
+++ map-manager.js
@@ -3,6 +3,8 @@ var mymap = L.map('mapid', { preferCanvas: true }).setView(
   16
 );
 
+const markerLayerGroup = L.layerGroup().addTo(mymap);
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -11,26 +13,48 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var governmentEntities = [
   'CITY & COUNTY OF DENVER',
   'UNITED STATES OF AMERICA',
+  'STATE OF COLORADO',
+  'DENVER SCHOOL FACILITIES LEASING CORPORATION',
+  'SCHOOL DISTRICT NO 1'
 ];
 
-addressInfo.forEach((addressWithInfo, index) => {
-  const { address, lat, long, ownerName, allOwnedProperties } = addressWithInfo;
-  try {
-    const marker = L.circleMarker([parseFloat(lat), parseFloat(long)], {
-      radius: 5,
-      ...(governmentEntities.includes(ownerName) && {
-        color: '#ff0000',
-        opacity: 0.5,
-      }),
-    }).addTo(mymap);
-    marker.bindPopup(
-      `${address} <br> <strong>Owned By</strong>: ${ownerName} <br> <strong>All Properties Owned</strong>: <br>${
-        allOwnedProperties.length > 25
-          ? allOwnedProperties.length
-          : allOwnedProperties.join(', ')
-      }`
-    );
-  } catch (err) {
-    console.log(index);
-  }
-});
+const setMarkers = (addressesToMap) => {
+  markerLayerGroup.clearLayers();
+  addressesToMap.forEach((addressWithInfo, index) => {
+    const {
+      address,
+      lat,
+      long,
+      ownerName,
+      allOwnedProperties,
+    } = addressWithInfo;
+    try {
+      const marker = L.circleMarker([parseFloat(lat), parseFloat(long)], {
+        radius: 5,
+        ...(governmentEntities.includes(ownerName) && {
+          color: '#ff0000',
+          opacity: 0.5,
+        }),
+      }).addTo(markerLayerGroup);
+      marker.bindPopup(
+        `${address} <br> <strong>Owned By</strong>: ${ownerName} <br> <strong>All Properties Owned</strong>: <br>${
+          allOwnedProperties.length > 25
+            ? allOwnedProperties.length
+            : allOwnedProperties.join(', ')
+        }`
+      );
+    } catch (err) {
+      console.log(index);
+    }
+  });
+};
+
+const updateMinimum = (value) => {
+  const filteredAddresses = addressInfo.filter(
+    ({ allOwnedProperties }) => allOwnedProperties.length >= value
+  );
+  setMarkers(filteredAddresses);
+};
+
+updateMinimum(document.querySelector('#minimumProperties').value)
+
